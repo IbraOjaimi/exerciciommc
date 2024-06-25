338c4fabeadd6358b9e91ed763ac5503eef26bc2
Alice-C-C <alicechimim@gmail.com>
2024-06-25 18:31:40 -0300

    melhorei essa merda

--- mmc.js
+++ mmc.js
@@ -1,18 +1,30 @@
-let num1=2
-let num2=5
+let num1
+let num2
+    
+console.log("Digite o primeiro número para saber o mmmc");
+    process.stdin.on('data', function(data){
+        num1=Number(data)
 
-function calcularMMC(num1, num2){
-    var resto, x, y;
-    x = num1;
-    y = num2;
+        console.log("Digite o segundo número:")
+        process.stdin.once('data', function(data){
+            function calcularMMC(num1, num2){
+                num2=Number(data)
+                var resto, x, y;
+                x = num1;
+                y = num2;
+            
+                while(resto!=0){
+                    resto = x % y;
+                    x = y;
+                    y = resto;
+                }
+            
+            return (num1*num2)/x;
+            }
+            var resultado =  calcularMMC(5, 10);
+            console.log(resultado)
+            process.exit()
 
-    while(resto!=0){
-        resto = x % y;
-        x = y;
-        y = resto;
-    }
 
-return (num1*num2)/x;
-}
-var resultado =  calcularMMC(5, 10);
-console.log(resultado)
+        })
+    })
